dac09718dee80a4d7bb81620f193ec78303c771a
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2023-12-16 01:39:44 +0100

    reusable component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { ThemeProvider } from '@material-ui/core/styles'
 import { Theme } from './commons/Assets/themes/theme'
 
 import PollWidget from './widget/pollWidget'
 import Layout from 'layout/Layout'
+import questionData from './questions.json'
+import questionData2 from './questionTwo.json'
+
 const loading = (
   <div className='pt-3 text-center'>
     <div className='sk-spinner sk-spinner-pulse'></div>
   </div>
 )
-function App () {
+
+function Home () {
+  const [widget1Open, setWidget1Open] = useState(false)
+  const [widget2Open, setWidget2Open] = useState(false)
+
+  const toggleWidget1 = () => {
+    setWidget1Open(!widget1Open)
+    if (widget2Open) {
+      setWidget2Open(false)
+    }
+  }
+
+  const toggleWidget2 = () => {
+    setWidget2Open(!widget2Open)
+    console.log(widget2Open)
+    if (widget1Open) {
+      setWidget1Open(false)
+    }
+  }
+
   return (
     <ThemeProvider theme={Theme}>
       <React.Suspense fallback={loading}>
         <Layout>
-          <PollWidget />
+          <div onClick={toggleWidget1}>
+            <h3>Widget 1</h3>
+          </div>
+          {widget1Open && (
+            <PollWidget questions={questionData} widgetId='widget1' />
+          )}
         </Layout>
       </React.Suspense>
     </ThemeProvider>
   )
 }
 
-export default App
+export default Home
